3e1e3585ce5da65fc03b65f6489089243e99b144
Or3yn <zxcbogdanova@gmail.com>
2024-05-18 11:23:21 +0300

    database init config

---
+++ src/test/java/com/librarymanagmentsystem/LMS/LibraryManagmentSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.librarymanagmentsystem.LMS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryManagmentSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
